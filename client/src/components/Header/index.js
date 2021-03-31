51aed904fe00c8080e2a14baee1e448b142d5a1a
Rebecca Blanton <beccaablanton@gmail.com>
2021-03-30 22:44:34 -0700

    refactored header component. added dropdown button for links in  smaller devices

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,6 +3,9 @@ import { Link, useLocation } from "react-router-dom";
 import bffl from "./smBffl.png"
 import AuthService from "../../services/authService";
 import "./style.css"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 const Header = () => {
   const [currentUser, setCurrentUser] = useState(undefined);
   const location = useLocation();
@@ -20,81 +23,40 @@ const Header = () => {
   };
 
     return (
-    <ul className="nav nav-tabs">
-      {currentUser ? (
-        <Link to="/matching" className="nav-link">
-          <img src={bffl} alt="bffl logo" width="80px" className="navbar-brand" />
-        </Link>
-      ) : (
-        <Link to="/" className="nav-link">
-            <img src={bffl} alt="bffl logo" width="80px" className="navbar-brand" />
-        </Link>
-      )}
-      
-      {currentUser ? (
-            <div className="navbar mr-auto">
-              <li className="nav-item">
-                <Link to={"/profile"} className={location.pathname === "/profile" ? "nav-link active" : "nav-link"}>
-                  {currentUser.username}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={logOut}>
-                  log Out
-                </a>
-              </li>
-            </div>
-          ) : (
-            <div className="navbar mr-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className={location.pathname === "/login" ? "nav-link active" : "nav-link"}>
-                  login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/register"} className={location.pathname === "/register" ? "nav-link active" : "nav-link"}>
-                  sign Up
-                </Link>
-              </li>
-            </div>
-          )}
-      
-      
-
-        {currentUser && (
-          <div className="navbar ml-auto">
-            <li className="nav-item">
-              <Link 
-               to="/user"
-               className={location.pathname === "/user" ? "nav-link active" : "nav-link"}>
-                account
-              </Link>
-            </li>
-          <li className="nav-item">
-            <Link
-              to="/matching"
-              className={location.pathname === "/matching" ? "nav-link active" : "nav-link"}>
-              start swiping
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/chat"
-              className={location.pathname === "/chat" ? "nav-link active" : "nav-link"}>
-              chat
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/matches"
-              className={location.pathname === "/favorites" ? "nav-link active" : "nav-link"}>
-              matches
-            </Link>
-          </li>
-        </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        {currentUser ? (
+          <Navbar.Brand href="/matching">
+              <img src={bffl} alt="bffl logo" width="80px"/>
+          </Navbar.Brand>
+        ) : (
+          <Navbar.Brand href="/">
+                <img src={bffl} alt="bffl logo" width="80px"/>
+          </Navbar.Brand>
         )}
-    </ul>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          
+          {currentUser ? (
+            <Nav className="mr-auto">
+                <Nav.Link href="/profile">{currentUser.username}</Nav.Link>
+                <Nav.Link href="/login" onClick={logOut}>log out</Nav.Link>
+            </Nav>
+                ) : (
+            <Nav className="mr-auto">
+                <Nav.Link href="/login">login</Nav.Link>
+                <Nav.Link href="/register">sign up</Nav.Link>
+            </Nav>
+                )}
+          {currentUser && (
+            <Nav className="ml-auto">
+              <Nav.Link href="/user">account</Nav.Link>
+              <Nav.Link href="/matching">start swiping</Nav.Link>
+              <Nav.Link href="/chat">chat</Nav.Link>
+              <Nav.Link href="/matches">matches</Nav.Link>
+            </Nav>
+            )}
+        </Navbar.Collapse>
+    </Navbar>
   );
 }
 
